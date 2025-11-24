0e39902881a94f3f21508f48c0c9027eb710bab1
Jon Lake <francis.j.lake@gmail.com>
2025-11-23 22:10:39 -0500

    Initial project setup

---
+++ main.py
@@ -0,0 +1,5 @@
+# Create a Typer application
+# Add a 'status' command that will check the server
+# Add a 'backup' command that will back up the world
+# Add a 'restart' command that will restart the server
+# Run the app at the bottom
