4a2ee826c9b29ec753df14cbbe6dfe49d81d7e08
Jon Lake <francis.j.lake@gmail.com>
2025-11-24 00:01:37 -0500

    add typer

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-# Create a Typer application
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello():
+    typer.echo("Hello world!")
+
+
+if __name__ == "__main__":
+    app()
+
+
 # Add a 'status' command that will check the server
 # Add a 'backup' command that will back up the world
 # Add a 'restart' command that will restart the server
-# Run the app at the bottom
